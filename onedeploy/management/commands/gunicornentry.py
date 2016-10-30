50639b0610ea2be3e73e66051f6d4e5386c540c6
Roger Camargo <huogerac@gmail.com>
2016-10-30 17:46:46 -0200

    Removed the URL from the Gunicorn file

--- onedeploy/management/commands/gunicornentry.py
+++ onedeploy/management/commands/gunicornentry.py
@@ -16,31 +16,26 @@ class Command(BaseCommand):
         make_option('--user',
                     action='store', dest='user', type="string",
                     help='Set the unix user that has permission to run gunicorn'),
-        make_option('--url',
-                    action='store', dest='url', type="string",
-                    help='Set the server url. e.g. yourdomain.com'),
         make_option('--site',
                     action='store', dest='site', type="string",
                     help='Set the Django SITE_ID'),
     )
     help = "Creates the Gunicorn conf file"
-    args = "--env --user --url --site"
+    args = "--env --user --site"
 
     def handle(self, *args, **options):
         """ """
         ROOT_DIR = str(settings.ROOT_DIR)
         PROJECT_DIR = str(settings.PROJECT_DIR)
-        PROJECT_NAME = basename(settings.PROJECT_DIR)
+        PROJECT_NAME = str(basename(abspath(PROJECT_DIR)))
         VIRTUALENV_DIR = abspath(join(ROOT_DIR, pardir))
 
         environment = options.get('env', '')
         user = options.get('user', '')
-        server_url = options.get('url', '')
         site_id = options.get('site', '')
 
         if not environment or \
-           not user or not server_url or \
-           not site_id:
+           not user or not site_id:
             self.stdout.write('---> ERROR: You must set all the parameters.\n')
             self.stdout.write('./manage.py gunicornentry --help\n')
             return
@@ -51,7 +46,6 @@ class Command(BaseCommand):
             'project_name_upper': PROJECT_NAME.upper(),
             'user': str(user),
             'site_id': site_id,
-            'server_url': server_url,
             'root_dir': ROOT_DIR,
             'virtualenv_folder': VIRTUALENV_DIR,
         }
